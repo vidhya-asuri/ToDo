37b1c2d31402f0208709096381c2a0314ccc2f2e
Vidhya <vid.sigsoln@gmail.com>
2019-02-12 22:53:46 -0500

    add a service and place all CRUD operation functionality in the service

--- src/app/todo.ts
+++ src/app/todo.ts
@@ -2,7 +2,7 @@ export class Todo {
 id: number;
 title: string = '';
 complete: boolean = false;
-
+// https://www.sitepoint.com/angular-2-tutorial/
 constructor(values:Object = {}){
 	Object.assign(this,values);
 }
